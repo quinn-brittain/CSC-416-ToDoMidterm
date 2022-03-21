94c98a4d480f47123422ff490b7ed0045b5603a8
Quinn Brittain <qgbritt@gmail.com>
2022-03-20 21:28:15 -0400

    Initial Commit

---
+++ ToDoMidterm/ToDoMidtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMidtermApp.swift
+//  ToDoMidterm
+//
+//  Created by Quinn Brittain on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMidtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
